63710fcd42fc72bbd004dac68213c6c0183d98d3
kaanbaba910 <100959483+kaanbaba910@users.noreply.github.com>
2024-06-15 13:59:57 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -8,7 +8,7 @@ export BRANCH="twrp-11" # the branch of manifest
 # about your device
 export DEVICE="X500" # codename used in device tree
 export DT_LINK="https://github.com/twrpdtgen/android_device_omix_X500" # device tree link
-export DT_BRANCH="twrp" # device tree branch
+export DT_BRANCH="android-11.0"
 export VENDOR="omix" # device manufacturer or vendor
 export TARGET="bootimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="aosp" # recovery type: omni, twrp, something else
