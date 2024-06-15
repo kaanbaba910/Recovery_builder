a59964e11262aaddf3f4dfc99fe1ed42949f6c8d
kaanbaba910 <100959483+kaanbaba910@users.noreply.github.com>
2024-06-15 09:41:50 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -11,7 +11,7 @@ export DT_LINK="https://github.com/twrpdtgen/android_device_omix_X500" # device 
 export DT_BRANCH="twrp" # device tree branch
 export VENDOR="omix" # device manufacturer or vendor
 export TARGET="bootimage" # build method: recoveryimage, bootimage, vendorboot
-export TYPE="omni" # recovery type: omni, twrp, something else
+export TYPE="twrp" # recovery type: omni, twrp, something else
 
 # you dont need to edit these
 export SYM="_"
